f7c8b24cfd169bb4b7b3adb4e888c42ef7241a56
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-01-17 19:36:26 +0330

    css-flexbox project

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".header__icon")
+let menuBtnIcon = document.querySelector(".header__icon i")
+
+menuBtn.addEventListener("click", function (){
+    menu.style.left = "0"
+    if (menuBtnIcon.classList.contains("fa-bars")){
+        menu.style.left = "0"
+        menuBtnIcon.classList = "fa fa-times"
+    } else {
+        menu.style.left = "-256px"
+        menuBtnIcon.classList = "fa fa-bars"
+    }
+})
